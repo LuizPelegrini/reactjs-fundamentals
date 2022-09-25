5f4ccbf32d68a370d36f7dc2ca2e210e127e9a9c
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2022-09-25 17:40:55 +0800

    add: Create Post component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { Post } from './components/Post';
+
 export function App() {
   return (
-    <h1>Hello World!</h1>
-  )
+    <div>
+      <Post
+        author="Luiz"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ipsam suscipit quos repellendus qui velit fugit, doloremque animi a corporis labore hic vel voluptatum incidunt tenetur sed nisi debitis molestiae!"
+      />
+  
+      <Post
+        author="Guilherme"
+        content="Another Post content"
+      />
+    </div>
+  );
 }
