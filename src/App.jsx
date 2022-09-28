3a2d97344e8d432f14a9e3f834e83f9ca3d0323a
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2022-09-28 21:42:17 +0800

    add: Create sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import { Post } from './components/Post';
 import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css'
 
 export function App() {
   return (
     <div>
       <Header />
 
-      <Post
-        author="Luiz"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ipsam suscipit quos repellendus qui velit fugit, doloremque animi a corporis labore hic vel voluptatum incidunt tenetur sed nisi debitis molestiae!"
-      />
-  
-      <Post
-        author="Guilherme"
-        content="Another Post content"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+        <Post
+          author="Luiz"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ipsam suscipit quos repellendus qui velit fugit, doloremque animi a corporis labore hic vel voluptatum incidunt tenetur sed nisi debitis molestiae!"
+        />
+    
+        <Post
+          author="Guilherme"
+          content="Another Post content"
+        />
+        </main>
+      </div>
     </div>
   );
 }
